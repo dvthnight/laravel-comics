2d281f914696571a6150960b5a71942dc138a006
dvthnight <davide.lanotte@protonmail.com>
2022-04-19 18:14:40 +0200

    fatto il dump dei fumetti

--- laravel/resources/views/pages/comics.blade.php
+++ laravel/resources/views/pages/comics.blade.php
@@ -4,6 +4,13 @@
 
 @section('content')
     <main>
-        ciao sono il main
+        <div class="immagine_jumbo"> </div>
+        <h3 class="titolo_serie_corrente">Current series</h3>
+        <div class="container">
+            <div class="fumetti">
+                @dump($fumetti)
+            </div>
+            <button class="load_button">Load More</button>
+        </div>
     </main>
 @endsection
--- laravel/resources/views/partials/header.blade.php
+++ laravel/resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-    ciao sono header
+    <div class="container">
+        <figure class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </figure>
+
+        <ul class="nav-list">
+            <li class="nav-element"><a href="#">Characters</a></li>
+            <li class="nav-element active"><a href="#">Comics</a></li>
+            <li class="nav-element"><a href="#">Movies</a></li>
+            <li class="nav-element"><a href="#">Tv</a></li>
+            <li class="nav-element"><a href="#">Games</a></li>
+            <li class="nav-element"><a href="#">Collectibles</a></li>
+            <li class="nav-element"><a href="#">Videos</a></li>
+            <li class="nav-element"><a href="#">Fans</a></li>
+            <li class="nav-element"><a href="#">News</a></li>
+            <li class="nav-element"><a href="#">Shop</a></li>
+        </ul>
+    </div>
 </header>
