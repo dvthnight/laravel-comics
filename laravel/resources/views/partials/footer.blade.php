db5c6850708bb9266ed57b1f5745724402f67ab3
dvthnight <davide.lanotte@protonmail.com>
2022-04-19 18:57:07 +0200

    modificato footer

--- laravel/resources/views/partials/footer.blade.php
+++ laravel/resources/views/partials/footer.blade.php
@@ -1,124 +1,122 @@
 <footer>
-    <div class="container">
-        <div class="blue_nav">
-           <div class="container">
+    <div class="blue_nav">
+        <div class="container">
 
-               <ul class="ul_blue_nav">
-                   <li class="el_blue_nav">
-                       <figure >
-                           <img class="img_blue_nav" src="/img/buy-comics-digital-comics.png" alt="">
-                       </figure>
-                       <a href="#" class="a_blue_nav">Digital comics</a>
-                    </li>
-                   <li class="el_blue_nav">
-                        <figure >
-                           <img class="img_blue_nav" src="/img/buy-comics-merchandise.png" alt="">
-                       </figure>
-                       <a href="#" class="a_blue_nav">Dc merchandise</a>
-                    </li>
-                   <li class="el_blue_nav">
-                        <figure >
-                           <img class="img_blue_nav"  src="/img/buy-comics-shop-locator.png" alt="">
-                       </figure>
-                       <a href="#" class="a_blue_nav">Subscription</a>
-                    </li>
-                   <li class="el_blue_nav">
-                        <figure>
-                           <img class="img_blue_nav" src="/img/buy-comics-subscriptions.png" alt="">
-                       </figure>
-                       <a href="#" class="a_blue_nav">Comic shop locator</a>
-                    </li>
-                   <li class="el_blue_nav">
-                        <figure >
-                           <img class="img_blue_nav power_visa" src="/img/buy-dc-power-visa.svg" alt="">
-                       </figure>
-                       <a href="#" class="a_blue_nav">Dc power visa</a>
-                    </li>
-               </ul>
+            <ul class="ul_blue_nav">
+                <li class="el_blue_nav">
+                    <figure >
+                        <img class="img_blue_nav" src="/img/buy-comics-digital-comics.png" alt="">
+                    </figure>
+                    <a href="#" class="a_blue_nav">Digital comics</a>
+                </li>
+                <li class="el_blue_nav">
+                    <figure >
+                        <img class="img_blue_nav" src="/img/buy-comics-merchandise.png" alt="">
+                    </figure>
+                    <a href="#" class="a_blue_nav">Dc merchandise</a>
+                </li>
+                <li class="el_blue_nav">
+                    <figure >
+                        <img class="img_blue_nav"  src="/img/buy-comics-shop-locator.png" alt="">
+                    </figure>
+                    <a href="#" class="a_blue_nav">Subscription</a>
+                </li>
+                <li class="el_blue_nav">
+                    <figure>
+                        <img class="img_blue_nav" src="/img/buy-comics-subscriptions.png" alt="">
+                    </figure>
+                    <a href="#" class="a_blue_nav">Comic shop locator</a>
+                </li>
+                <li class="el_blue_nav">
+                    <figure >
+                        <img class="img_blue_nav power_visa" src="/img/buy-dc-power-visa.svg" alt="">
+                    </figure>
+                    <a href="#" class="a_blue_nav">Dc power visa</a>
+                </li>
+            </ul>
 
+        </div>
+    </div>
+        <div class="footer_centrale">
+            <div class="container">
+
+                <div class="centrale_colonne">
+                    <div class="colonna1 col_foot">
+                        <div class="ul_1_sx col_foot">
+                            <h3 class="titolo_colonne_footer">Dc Comics</h3>
+                            <li class="el_foot_centr"><a href="#">Characters</a></li>
+                            <li class="el_foot_centr"><a href="#">Comics</a></li>
+                            <li class="el_foot_centr"><a href="#">Movies</a></li>
+                            <li class="el_foot_centr"><a href="#">TV</a></li>
+                            <li class="el_foot_centr"><a href="#">Games</a></li>
+                            <li class="el_foot_centr"><a href="#">Videos</a></li>
+                            <li class="el_foot_centr"><a href="#">News</a></li>
+                        </div>
+
+                        <div class="ul_2_sx col_foot">
+                            <h3 class="titolo_colonne_footer">Shop</h3>
+                            <li class="el_foot_centr"><a href="#">Shop DC</a></li>
+                            <li class="el_foot_centr"><a href="#">Shop DC Collectibles</a></li>
+                        
+                        </div>
+                    </div>
+
+                    <div class="colonna2 col_foot">
+                        <h3 class="titolo_colonne_footer">DC</h3>
+                        <li class="el_foot_centr"><a href="#">Terms Of Use</a></li>
+                        <li class="el_foot_centr"><a href="#">Privacy policy (New)</a></li>
+                        <li class="el_foot_centr"><a href="#">Ad Choices</a></li>
+                        <li class="el_foot_centr"><a href="#">Advertising</a></li>
+                        <li class="el_foot_centr"><a href="#">Jobs</a></li>
+                        <li class="el_foot_centr"><a href="#">Subscriptions</a></li>
+                        <li class="el_foot_centr"><a href="#">Talent Workshop</a></li>
+                        <li class="el_foot_centr"><a href="#">CPSC Certicates</a></li>
+                        <li class="el_foot_centr"><a href="#">Ratings</a></li>
+                        <li class="el_foot_centr"><a href="#"></a>Shop Help</li>
+                        <li class="el_foot_centr"><a href="#">Contact Us</a></li>
+                    </div>
+
+                    <div class="colonna3 col_foot">
+                        <h3 class="titolo_colonne_footer">Sites</h3>
+                        <li class="el_foot_centr"><a href="#">DC</a></li>
+                        <li class="el_foot_centr"><a href="#">MAD Magazine</a></li>
+                        <li class="el_foot_centr"><a href="#">DC Kids</a></li>
+                        <li class="el_foot_centr"><a href="#">DC Universe</a></li>
+                        <li class="el_foot_centr"><a href="#">DC Power Vista</a></li>
+                    </div>
+
+                </div>
+
+                <div class="centrale_logo">
+                    
+                </div>
             </div>
         </div>
-            <div class="footer_centrale">
-                <div class="container">
-
-                    <div class="centrale_colonne">
-                        <div class="colonna1 col_foot">
-                            <div class="ul_1_sx col_foot">
-                                <h3 class="titolo_colonne_footer">Dc Comics</h3>
-                                <li class="el_foot_centr"><a href="#">Characters</a></li>
-                                <li class="el_foot_centr"><a href="#">Comics</a></li>
-                                <li class="el_foot_centr"><a href="#">Movies</a></li>
-                                <li class="el_foot_centr"><a href="#">TV</a></li>
-                                <li class="el_foot_centr"><a href="#">Games</a></li>
-                                <li class="el_foot_centr"><a href="#">Videos</a></li>
-                                <li class="el_foot_centr"><a href="#">News</a></li>
-                            </div>
-
-                            <div class="ul_2_sx col_foot">
-                                <h3 class="titolo_colonne_footer">Shop</h3>
-                                <li class="el_foot_centr"><a href="#">Shop DC</a></li>
-                                <li class="el_foot_centr"><a href="#">Shop DC Collectibles</a></li>
-                            
-                            </div>
-                        </div>
-
-                        <div class="colonna2 col_foot">
-                            <h3 class="titolo_colonne_footer">DC</h3>
-                            <li class="el_foot_centr"><a href="#">Terms Of Use</a></li>
-                            <li class="el_foot_centr"><a href="#">Privacy policy (New)</a></li>
-                            <li class="el_foot_centr"><a href="#">Ad Choices</a></li>
-                            <li class="el_foot_centr"><a href="#">Advertising</a></li>
-                            <li class="el_foot_centr"><a href="#">Jobs</a></li>
-                            <li class="el_foot_centr"><a href="#">Subscriptions</a></li>
-                            <li class="el_foot_centr"><a href="#">Talent Workshop</a></li>
-                            <li class="el_foot_centr"><a href="#">CPSC Certicates</a></li>
-                            <li class="el_foot_centr"><a href="#">Ratings</a></li>
-                            <li class="el_foot_centr"><a href="#"></a>Shop Help</li>
-                            <li class="el_foot_centr"><a href="#">Contact Us</a></li>
-                        </div>
-
-                        <div class="colonna3 col_foot">
-                            <h3 class="titolo_colonne_footer">Sites</h3>
-                            <li class="el_foot_centr"><a href="#">DC</a></li>
-                            <li class="el_foot_centr"><a href="#">MAD Magazine</a></li>
-                            <li class="el_foot_centr"><a href="#">DC Kids</a></li>
-                            <li class="el_foot_centr"><a href="#">DC Universe</a></li>
-                            <li class="el_foot_centr"><a href="#">DC Power Vista</a></li>
-                        </div>
-
-                    </div>
-
-                    <div class="centrale_logo">
-                        
-                    </div>
-                </div>
+    <div class="footer_basso">
+        <div class="container">
+            <div class="button_sign_foot">
+                <button class="btn_sign">Sign-up Now</button>
             </div>
-        <div class="footer_basso">
-            <div class="container">
-                <div class="button_sign_foot">
-                    <button class="btn_sign">Sign-up Now</button>
-                </div>
-                <div class="social">
-                    <span class="follow">Follow Us</span>
+            <div class="social">
+                <span class="follow">Follow Us</span>
 
-                    <ul class="lista_social">
-                        <li class="social_logo"><a href="#">
-                            <img src="/img/footer-facebook.png" alt="">
-                            </a></li>
-                        <li class="social_logo"><a href="#">
-                            <img src="/img/footer-twitter.png" alt="">
-                            </a></li>
-                        <li class="social_logo"><a href="#">
-                            <img src="/img/footer-youtube.png" alt="">
-                            </a></li>
-                        <li class="social_logo"><a href="#">
-                            <img src="/img/footer-pinterest.png" alt="">
-                            </a></li>
-                        <li class="social_logo"><a href="#">
-                            <img src="/img/footer-periscope.png" alt="">
-                            </a></li>
-                    </ul>
-                </div>
+                <ul class="lista_social">
+                    <li class="social_logo"><a href="#">
+                        <img src="/img/footer-facebook.png" alt="">
+                        </a></li>
+                    <li class="social_logo"><a href="#">
+                        <img src="/img/footer-twitter.png" alt="">
+                        </a></li>
+                    <li class="social_logo"><a href="#">
+                        <img src="/img/footer-youtube.png" alt="">
+                        </a></li>
+                    <li class="social_logo"><a href="#">
+                        <img src="/img/footer-pinterest.png" alt="">
+                        </a></li>
+                    <li class="social_logo"><a href="#">
+                        <img src="/img/footer-periscope.png" alt="">
+                        </a></li>
+                </ul>
             </div>
         </div>
     </div>
